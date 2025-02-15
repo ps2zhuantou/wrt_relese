d292fac682f9da58b7df3d30c93744a7fe3be8c1
ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>
2025-02-15 16:58:59 +0800

    Update update.sh
    Signed-off-by: ps2zhuantou <30466825+ps2zhuantou@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -130,7 +130,6 @@ install_small8() {
     ./scripts/feeds install -p small8 -f xray-core xray-plugin dns2tcp dns2socks haproxy hysteria \
         naiveproxy shadowsocks-rust sing-box v2ray-core v2ray-geodata v2ray-geoview v2ray-plugin \
         tuic-client chinadns-ng ipt2socks tcping trojan-plus simple-obfs shadowsocksr-libev \
-        ipset iptables-nft arptables-nft ebtables-nft xtables-nft \
         luci-app-passwall alist luci-app-alist smartdns luci-app-smartdns v2dat mosdns luci-app-mosdns \
         adguardhome luci-app-adguardhome ddns-go luci-app-ddns-go taskd luci-lib-xterm luci-lib-taskd \
         luci-app-store quickstart luci-app-quickstart luci-app-istorex luci-app-cloudflarespeedtest \
